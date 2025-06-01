6d673560f2aeee742f7d834bab9e1c9ac4f9949d
Tarie123 <tarymhlang@gmail.com>
2025-06-01 21:06:44 +0200

    fixed

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05 
 time = 3 
 
-intrest = principal * rate * time
+intrests = principal * rate * time
 
-print ("The simple interest is:", intrest)
+print ("The simple interest is:", intrests)
